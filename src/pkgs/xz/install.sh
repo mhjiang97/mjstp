cdfe5bf8f156889772f9e09ab3e3d09c2316b87c
Minghao Jiang | Office <jiangminghao1001@163.com>
2026-06-26 11:47:49 +0800

    Update README and installation scripts: add xz as a dependency for bcftools and create a new installation script for xz

--- src/pkgs/xz/install.sh
+++ src/pkgs/xz/install.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-# Dependencies: htslib, xz
+# Dependencies:
 set -e
 
 # shellcheck disable=SC1091
 [ -n "${LIB_DIR}" ] && source "${LIB_DIR}/utils.sh"
 
-VERSION="1.23"
+VERSION="5.8.3"
 INSTALL_DIR="${HOME}/local"
 
-if [ -x "${INSTALL_DIR}/bin/bcftools" ]; then
+if [ -x "${INSTALL_DIR}/bin/xz" ]; then
     if [ -n "${MJSTP_UPDATE}" ]; then
-        log_info "Reinstalling bcftools ${VERSION}..."
+        log_info "Reinstalling xz ${VERSION}..."
     else
-        log_info "bcftools ${VERSION} is already installed."
+        log_info "xz ${VERSION} is already installed at ${INSTALL_DIR}."
         exit 0
     fi
 fi
 
-log_info "Installing bcftools ${VERSION}..."
+log_info "Installing xz ${VERSION}..."
 
-TARBALL="bcftools-${VERSION}.tar.bz2"
-URL="https://github.com/samtools/bcftools/releases/download/${VERSION}/${TARBALL}"
+TARBALL="xz-${VERSION}.tar.gz"
+URL="https://github.com/tukaani-project/xz/releases/download/v${VERSION}/${TARBALL}"
 
 tmp_dir=$(mktemp -d)
 trap 'rm -rf "${tmp_dir}"' EXIT
@@ -30,12 +30,12 @@ log_info "Downloading ${URL}..."
 download "${URL}"
 
 log_info "Extracting..."
-tar xjf "${TARBALL}"
-cd "bcftools-${VERSION}" || exit 1
+tar -xzf "${TARBALL}"
+cd "xz-${VERSION}" || exit 1
 
 log_info "Configuring and building..."
 ./configure --prefix="${INSTALL_DIR}"
 make -j"$(nproc 2>/dev/null || echo 1)"
 make install
 
-log_success "bcftools ${VERSION} installed to ${INSTALL_DIR}."
+log_success "xz ${VERSION} installed to ${INSTALL_DIR}."
